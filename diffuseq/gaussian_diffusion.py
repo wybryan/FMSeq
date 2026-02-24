95824a56e05a3178511b03b5c8e9b900e2c4d318
wybryan <wybryan@hotmail.com>
2026-02-24 16:34:02 +0800

    resume to original nll loss

--- diffuseq/gaussian_diffusion.py
+++ diffuseq/gaussian_diffusion.py
@@ -151,6 +151,7 @@ class GaussianDiffusion:
         self.use_kl = use_kl
         self.loss_mask = kwargs["loss_mask"]
         self.pad_token_id = kwargs["pad_token_id"]
+        self.dynamic_batching = kwargs.get("dynamic_batching", False)
 
         # Use float64 for accuracy.
         betas = np.array(betas, dtype=np.float64)
@@ -710,9 +711,10 @@ class GaussianDiffusion:
         # out_mean, _, _ = self.q_mean_variance(x_start, th.LongTensor([self.num_timesteps - 1]).to(x_start.device))
         # tT_loss =  mean_flat(out_mean ** 2)
 
-        nll_mask = th.where(input_ids_x == self.pad_token_id, 0, input_ids_mask)
-        decoder_nll = self._token_discrete_loss(x_start, get_logits, input_ids_x, mask=nll_mask) # embedding regularization
-        terms["nll"] = self._token_discrete_loss(model_out_x_start, get_logits, input_ids_x, mask=nll_mask, truncate=True, t=t) # x_0->model_out_x_start
+        # decoder_nll = self._token_discrete_loss(x_start, get_logits, input_ids_x) # embedding regularization
+        decoder_nll = self._token_discrete_loss(x_start, get_logits, input_ids_x) # embedding regularization
+
+        terms["nll"] = self._token_discrete_loss(model_out_x_start, get_logits, input_ids_x, mask=input_ids_mask, truncate=True, t=t) # x_0->model_out_x_start
         # assert (model.lm_head.weight == model.word_embedding.weight).all()
 
         terms["loss"] = terms["mse"] + decoder_nll
